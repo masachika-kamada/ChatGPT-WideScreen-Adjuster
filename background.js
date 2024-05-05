daa891af3bb8b7cf09260d1875c74fff0db4a54a
Masachika Kamada <63488322+masachika-kamada@users.noreply.github.com>
2024-05-05 16:27:35 +0900

    v1.0.1 fix url and popup

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && tab.url) {
-    if (tab.url.startsWith('https://chat.openai.com/')) {
+    if (tab.url.startsWith('https://chatgpt.com/')) {
       chrome.action.enable(tabId);
     } else {
       chrome.action.disable(tabId);
